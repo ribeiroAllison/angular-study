be3ae775f630fef4caee648771be112f7e7c7d91
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2024-09-16 08:51:31 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wishlist';
+  items : WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffe', true),
+    new WishItem('Be Rich', false),
+  ];
+
+  title = 'my wishlist';
+
+  toogleItem(item : WishItem){
+    item.isComplete = !item.isComplete
+    console.log(item)
+  };
 }
