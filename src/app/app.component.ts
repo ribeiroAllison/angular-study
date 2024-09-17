06c8f1e8bccec1575142d418c880d9d72a78a163
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2024-09-17 08:40:57 -0300

    implement a simple to do list in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,30 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   items : WishItem[] = [
-    new WishItem('To Learn Angular'),
-    new WishItem('Get Coffe', true),
-    new WishItem('Be Rich', false),
+    // new WishItem('To Learn Angular'),
+    // new WishItem('Get Coffe', true),
+    // new WishItem('Be Rich', false),
   ];
 
   title = 'my wishlist';
+
+  newWishText = ""
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText))
+    this.newWishText= ""
+  }
 
   toogleItem(item : WishItem){
     item.isComplete = !item.isComplete
